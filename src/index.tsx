b2300ef24fbbf05572d36ae25cbe4401006b83c3
Imran Khan <impathan007@gmail.comwq>
2025-04-08 15:25:01 +0300

    feat: added layouts

--- src/index.tsx
+++ src/index.tsx
@@ -2,3 +2,4 @@ export * from './components/Button';
 export * from './components/Text';
 export * from './layouts/GridLayout';
 export * from './layouts/LinearLayout';
+export * from './components/Card';
